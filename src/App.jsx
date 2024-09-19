d15155340daea74099093d598feb2643dccab481
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-19 15:26:36 -0400

    add assets and basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TicTacToe from './components/tictactoe/tictactoe'
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/tictactoe/tictactoe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './tictactoe.css'
+
+const tictactoe = () => {
+  return (
+    <div>tictactoe</div>
+  )
+}
+
+export default tictactoe
